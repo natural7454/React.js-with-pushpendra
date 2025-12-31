6d37f85383ba07941fa8ab48721f1d62080106fb
Pushpendra Kumar <pushpendrakumar7454981358@gmail.com>
2025-12-31 13:04:52 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,19 +1,90 @@
-import React from 'react'
-
-import Profile from './componets/Profile'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div className="parent">
-      <Profile name="Amazon" days="5 Days Ago "senior="Senior UI|US Designer"part="Part-Time"level="Senior Level"rate="$150/hr"space="Mumbai India"img="https://tse2.mm.bing.net/th/id/OIP.moSCocxuD-WLoRTCzZm-8wHaGO?pid=Api&P=0&h=180"/>
-      <Profile name="Google" days="10 Days Ago"senior="Graphic Designer"part="Part-Time"level="Fiexi Schedu"rate="$120/hr"space="Delhi India"img="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png" />
-      <Profile name="Flipcart" days="15 Days Ago"senior="IT Designer"part="Part-Time"level="Juniur "rate="$100/hr"space="Suratgarh India"img="https://tse2.mm.bing.net/th/id/OIP.oXkipQKT-BAB4MsrxSnFgwHaEK?pid=Api&P=0&h=180" />
-      <Profile name="Google" days="10 Days Ago"senior="Graphic Designer"part="Part-Time"level="Fiexi Schedu"rate="$120/hr"space="Delhi India"img="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png" /><Profile name="Google" days="10 Days Ago"senior="Graphic Designer"part="Part-Time"level="Fiexi Schedu"rate="$120/hr"space="Delhi India"img="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png" /><Profile name="Google" days="10 Days Ago"senior="Graphic Designer"part="Part-Time"level="Fiexi Schedu"rate="$120/hr"space="Delhi India"img="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png" />
-    </div>
-  )
-}
+  const [title, settitle] = useState('');
+  const [detail, setdetail] = useState('');
+  const [task, setask] = useState([]);
 
-export default App
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const CopyTask = [...task];
+    CopyTask.push({ title, detail });
+    setask(CopyTask);
+
+    settitle('');
+    setdetail('');
+  };
+
+  // ✅ delete handler
+  const deleteHandler = (index) => {
+    const copyTask = [...task];
+    copyTask.splice(index, 1);
+    setask(copyTask);
+  };
+
+  return (
+    <div>
+      <div className="parent">
+        <form onSubmit={submitHandler}>
+          <h1 className="add">Add Notes</h1>
+
+          <div>
+            <input
+              type="text"
+              placeholder="Enter you Text"
+              value={title}
+              onChange={(e) => {
+                settitle(e.target.value);
+              }}
+            />
+          </div>
+
+          <div>
+            <textarea
+              placeholder="Write detail"
+              value={detail}
+              onChange={(e) => {
+                setdetail(e.target.value);
+              }}
+            ></textarea>
+
+            <div>
+              <button type="submit"id="btn">Add Note</button>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div>
+        <h1 className="notes">Recent Notes</h1>
+      </div>
+
+      <div className="line"></div>
+
+      <div className="flex">
+        {task.map((item, index) => {
+          return (
+            <div className="Firstdiv" key={index}>
+              <h2>{item.title}</h2>
+              <p>{item.detail}</p>
+
+              {/* ✅ delete button */}
+              <button onClick={() => deleteHandler(index)} className="delete">
+                Delete Note
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+
+
 
 
 
